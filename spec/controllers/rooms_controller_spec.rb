79ab7b8f9a2e5944d9a700c9ac9b34ab484fa63d
Austin Strom <austin-strom@uiowa.edu>
2020-11-06 13:14:08 -0600

    Updated specs to better reflect how controller actions should be implemented. Removed expectation for calling new method on model

--- spec/controllers/rooms_controller_spec.rb
+++ spec/controllers/rooms_controller_spec.rb
@@ -3,17 +3,16 @@ require 'rails_helper'
 
 describe RoomsController do
   describe 'creating a new room' do
-    it 'should call the method that creates a new room' do
-      expect(Room).to receive(:new)
-      post :create
-    end
     it 'should successfully create a new room' do
       post :create
       expect(Room.all.count).to eq(1)
     end
     it 'should redirect to the new created room' do
       post :create
-      expect(response).to render_template(room_path)
+      expect(response).to render_template(room_path/1)
+    end
+    it 'should generate a room code' do
+
     end
   end
 end
