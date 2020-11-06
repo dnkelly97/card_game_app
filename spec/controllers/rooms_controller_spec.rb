590a65ebc93c391b1be04013ce5b9d06ebd036d5
Austin Strom <austin-strom@uiowa.edu>
2020-11-06 12:50:38 -0600

    Fixing up tests before implementation for correct routes and expectations

--- spec/controllers/rooms_controller_spec.rb
+++ spec/controllers/rooms_controller_spec.rb
@@ -4,10 +4,15 @@ require 'rails_helper'
 describe RoomsController do
   describe 'creating a new room' do
     it 'should call the method that creates a new room' do
-      expect(Room).to receive(:new).and_return("Fake Room")
+      expect(Room).to receive(:new)
+      post :create
+    end
+    it 'should successfully create a new room' do
+      post :create
+      expect(Room.all.count).to eq(1)
     end
     it 'should redirect to the new created room' do
-      post :create_room
+      post :create
       expect(response).to render_template(room_path)
     end
   end
