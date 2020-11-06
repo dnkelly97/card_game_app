74dbc88455fd6877b69c548114ef44aa31297e9c
Austin Strom <austin-strom@uiowa.edu>
2020-11-06 15:10:20 -0600

    Adding parameter function for gettings params from forms

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,10 +1,14 @@
 class RoomsController < ApplicationController
+  def room_params
+    params.require(:room).permit(:name)
+  end
+
   def new
     @room = Room.new
   end
 
   def create
-    @room = Room.create!
+    @room = Room.create!()
     redirect_to room_path(@room)
   end
 
