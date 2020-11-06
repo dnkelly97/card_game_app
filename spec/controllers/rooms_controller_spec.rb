fcc830840757052cf17c789606b5dfd2cb3d81b0
Austin Strom <austin-strom@uiowa.edu>
2020-11-06 12:23:23 -0600

    Adding basic tests for creating a new room and redirecting to that room

--- spec/controllers/rooms_controller_spec.rb
+++ spec/controllers/rooms_controller_spec.rb
@@ -1,4 +1,14 @@
 require 'spec_helper'
 require 'rails_helper'
 
-
+describe RoomsController do
+  describe 'creating a new room' do
+    it 'should call the method that creates a new room' do
+      expect(Room).to receive(:new).and_return("Fake Room")
+    end
+    it 'should redirect to the new created room' do
+      post :create_room
+      expect(response).to render_template(room_path)
+    end
+  end
+end
