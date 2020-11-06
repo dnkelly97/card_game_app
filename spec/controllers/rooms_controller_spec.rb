1a0edba4516ceddd469f9d9e1c5fe3014a837528
Austin Strom <austin-strom@uiowa.edu>
2020-11-06 15:14:45 -0600

    Added checking for proper room naming to specs

--- spec/controllers/rooms_controller_spec.rb
+++ spec/controllers/rooms_controller_spec.rb
@@ -4,11 +4,12 @@ require 'rails_helper'
 describe RoomsController do
   describe 'creating a new room' do
     it 'should successfully create a new room' do
-      post :create
+      post :create, {:room_name => 'Test'}
       expect(Room.all.count).to eq(1)
+      expect(Room.find(1).name).to eq('Test')
     end
     it 'should redirect to the new created room' do
-      post :create
+      post :create, {:room_name => 'Test'}
       expect(response).to redirect_to('/rooms/1')
       #TODO: Figure out how to change to a path variable
     end
