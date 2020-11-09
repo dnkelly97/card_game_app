dbbb438ef744b375839fd03df622fa5e8a1e8340
Austin Strom <austin-strom@uiowa.edu>
2020-11-09 11:52:26 -0600

    Adding migration for adding room attributes

---
+++ db/migrate/20201109174448_add_more_selectable_room_attributes.rb
@@ -0,0 +1,7 @@
+class AddMoreSelectableRoomAttributes < ActiveRecord::Migration
+  def change
+    add_column :rooms, :max_players, :integer
+    add_column :rooms, :game_type, :string
+    add_column :rooms, :private, :boolean
+  end
+end
