d99a0ac7acc55332a4bba1f9bb7467ee514d005c
Austin Strom <austin-strom@uiowa.edu>
2020-11-09 15:22:22 -0600

    Updating room code generation

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,7 @@
 class Room < ActiveRecord::Base
+  #TODO: Add validations to handle invalid or empty inputs
+  def self.create_room!(room_params)
+    room_params[:room_code] = SecureRandom.random_number(9999999999)
+    Room.create!(room_params)
+  end
 end
