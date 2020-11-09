2b018a814af9d200e10d8380a4a2bfb837e62db4
KaiwenS7 <kevsteele7@gmail.com>
2020-11-09 11:21:33 -0600

    basic functionality for login done (no tests)

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,6 +1,29 @@
 class UserSessionsController < ApplicationController
   def session_params
-    params.require(:user).permit(:user_id, :password)
+    params.require(:user).permit(:email, :password)
   end
 
+  def create
+    if session_params.has_key?(:password) and session_params.has_key?(:email)
+      # Check that the combination gets a user with both
+
+      @a_user = User.where({ email: session_params[:email] }).to_a[0]
+      if @a_user.authenticate( session_params[:password] )
+        session[:session_token] = @a_user.session_token
+        flash[:notice] = "Welcome. Your account was successfully logged in."
+        redirect_to dashboard_path
+        return
+      end
+    end
+
+    flash[:notice] = "Unsuccessful login"
+    redirect_to login_path
+  end
+
+  def destroy
+    session[:session_token] = nil
+    flash[:notice] = "Successfully logged out."
+    # Maybe should change the session token for next time
+    redirect_to login_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,11 @@ Rails.application.routes.draw do
   match '/login', to: 'user_sessions#new', via: :get
   match '/login', to: 'user_sessions#create', via: :post
   match '/logout', to: 'user_sessions#destroy', via: :post
+  match '/dashboard', to: 'user_sessions#index', via: :get
   # get  'auth/:provider/callback' => 'user_sessions#create'
   # get  'auth/failure' => 'user_sessions#failure'
 
-  root to: redirect('/login')
+  root to: redirect('/dashboard')
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
