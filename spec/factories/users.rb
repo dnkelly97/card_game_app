3e8f9ea254790a18370f1512a14d9e5bcaada8aa
KaiwenS7 <kevsteele7@gmail.com>
2020-11-09 15:14:13 -0600

    added spec for sessions controller

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,9 @@
 FactoryGirl.define do
   factory :user do
-    id { 1 }
-    email { 'you@me.com' }
-    password { '' }
+    email { 'me@you.com' }
+    email_confirmation { 'me@you.com' }
+    password { 'testpass1' }
+    password_confirmation { 'testpass1' }
+    user_id { 'kevin' }
   end
 end
