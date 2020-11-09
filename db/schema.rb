716419b1287d3ed6f64363a4a30d20bb7ba00fc5
Austin Strom <austin-strom@uiowa.edu>
2020-11-08 19:25:56 -0600

    Updated schema for adding details to rooms

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201106174649) do
+ActiveRecord::Schema.define(version: 20201106221001) do
 
   create_table "rooms", force: :cascade do |t|
     t.string   "name"
