20f95635e4ef4651e478ab705c0cc10f4c695b4f
dnkelly97 <dnkelly97@gmail.com>
2020-11-09 01:15:18 -0600

    made sad path for create_join, but flash doesn't display yet

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -20,7 +20,12 @@ class RoomsController < ApplicationController
   end
 
   def create_join
-    @room = Room.find(params[:id])
-    redirect_to room_path(@room)
+    begin
+      @room = Room.find(params[:id])
+      redirect_to room_path(@room)
+    rescue ActiveRecord::RecordNotFound
+      flash[:warning] = "A room with that code does not exist."
+      redirect_to rooms_new_join_path
+    end
   end
 end
