f254495a109f1c44faa509fdc9e814cca7a64964
Austin Strom <austin-strom@uiowa.edu>
2020-11-10 17:02:38 -0600

    Adding base connection file for client-server

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,7 +1,9 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    # indentified_by :uuid
     def connect
       # TODO: Add user verification
+      # self.uuid = SecureRandom.urlsafe_base64
     end
   end
 end
