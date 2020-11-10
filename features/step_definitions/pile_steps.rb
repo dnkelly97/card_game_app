098542ac64701607df7c40a0179844bcd424ad05
Gabriel Douglass <gabrieljdouglass@gmail.com>
2020-11-10 14:08:57 -0600

    Added a step for 'given that a pile exists in the database'

--- features/step_definitions/pile_steps.rb
+++ features/step_definitions/pile_steps.rb
@@ -25,3 +25,59 @@ Then /^I should see a piles list entry with name "(.*?)" and creator "(.*?)" and
   expect(result).to be_truthy
 end
 
+Given ('that at least one pile exists in the database') do
+  visit create_pile_path
+  fill_in 'Pile Name:', :with => "Deck"
+  fill_in 'Your Name:', :with => "The Game"
+  select true, :from => 'private_pile'
+  click_button 'Create my pile!'
+end
+
+
+
+And /^the following cards have been added to the pile whose id is (.*?):$/ do |float, cards_table|
+  cards_table.hashes.each do |card|
+    card[:pile_id] = float
+    Card.create!(card)
+  end
+
+end
+
+And /^I have tried to transfer the (.*?) from pile with id=(.*?) to pile with name "(.*?)"$/ do |card, source_pile_id, destination_pile|
+  visit root_path
+  source_pile = Pile.find_by(id: source_pile_id)
+  log(source_pile.name)
+  fill_in 'Enter Source Pile:', :with => source_pile.name
+  click_button 'Transfer Card'
+  log(page.body)
+  check "checkbox_#{card}"
+  fill_in 'Destination Pile:', :with => destination_pile
+  log(destination_pile)
+  click_button 'Transfer Card(s)'
+  log(page.body)
+end
+
+Then /^I should see the (.*?) in the previously empty pile: "(.*?)"$/ do |card, pile_name|
+  visit root_path
+  fill_in 'Enter Source Pile:', :with => pile_name
+  click_button 'Transfer Card'
+  card = Card.find_by(name: card)
+  #log(card)
+  card_id = card.id
+  # log(card_id)
+  #expect(page).to have_content(card_id)
+
+  result=false
+  all("tr").each do |tr|
+    #log(page.body)
+    if tr.has_content?(card_id)
+      result = true
+      break
+    end
+  end
+  expect(result).to be_truthy
+
+end
+
+
+
