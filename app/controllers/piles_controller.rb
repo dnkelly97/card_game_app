c386dd817eea0caac72cb7753b20ec55022af4bf
Gabriel Douglass <gabrieljdouglass@gmail.com>
2020-11-11 17:08:22 -0600

    Made it so that when transferring cards or given invalid form info we are redirected to game room rather than piles_pile_homepage

--- app/controllers/piles_controller.rb
+++ app/controllers/piles_controller.rb
@@ -22,9 +22,10 @@ class PilesController < ApplicationController
   def show
     source_pile = params[:pile][:source_pile]
     source_pile = Pile.find_by(name: source_pile)
+    room_id = params[:room_id]
     if source_pile.nil?
       flash[:notice] = "This is not a pile in the database. Please try again." #@pile.name
-      redirect_to piles_pile_homepage_path and return
+      redirect_to room_path({:id => room_id}) and return
     end
     @is_private = source_pile.private_pile
     @cards = source_pile.cards
@@ -32,21 +33,22 @@ class PilesController < ApplicationController
 
   def transfer_card
     destination_pile = params[:pile][:name2]
+    room_id = params[:room_id]
     @destination_pile = Pile.find_by(name: destination_pile)
     if @destination_pile.nil?
       flash[:notice] = "This is not a pile in the database. Please try again." #@pile.name
-      redirect_to piles_pile_homepage_path and return
+      redirect_to room_path({:id => room_id}) and return #piles_pile_homepage_path and return
     end
     if params[:the_cards].nil?
       flash[:notice] = "No cards selected"
-      redirect_to piles_path and return
+      redirect_to room_path({:id => room_id}) and return
     end
     params[:the_cards].keys.each do |card|
       flash[:notice] = "#{card}"
       @destination_pile.cards << Card.find_by(name: card)
     end
     flash[:notice] = "Card(s) successfully transferred!"
-    redirect_to piles_path
+    redirect_to room_path({:id => room_id})
   end
 
   def pile_homepage
