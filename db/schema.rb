90c71e0b2b689ba3200dd56a4843b0c7f8f57af4
Gabriel Douglass <gabrieljdouglass@gmail.com>
2020-11-11 16:44:08 -0600

    Removed an extra space

--- db/schema.rb
+++ db/schema.rb
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 20201109210922) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
-    create_table "cards", force: :cascade do |t|
+  create_table "cards", force: :cascade do |t|
     t.string  "name"
     t.integer "pile_id"
   end
