2164749337747670657a6640d72b4a9b248e697e
Austin Strom <austin-strom@uiowa.edu>
2020-11-10 22:39:13 -0600

    Updated rspec for switching to room codes

--- spec/controllers/rooms_controller_spec.rb
+++ spec/controllers/rooms_controller_spec.rb
@@ -17,20 +17,22 @@ describe RoomsController do
   end
   describe 'joining a new room' do
     before(:each) do
+      allow(SecureRandom).to receive(:random_number).and_return(1111111111)
       post :create, {:room_name => "Dan's Test Room"}
     end
     it 'should redirect to the room page corresponding to the valid id given' do
-      post :create_join, {:id => 1}
+
+      post :create_join, {:id => 1111111111}
       # byebug
       expect(response).to redirect_to('/rooms/1')
     end
     it 'should redirect to the join room page if invalid id is given' do
-      post :create_join, {:id => 10000221}
+      post :create_join, {:id => 1111111110}
       # byebug
       expect(response).to redirect_to('/rooms/new_join')
     end
     it 'should flash a warning if an invalid id is given' do
-      post :create_join, {:id => 10000221}
+      post :create_join, {:id => 1111111110}
       # byebug
       expect(flash[:warning]).to eq("A room with that code does not exist.")
     end
