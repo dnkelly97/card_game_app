92b655db15d84a558a149875563f2d91b83c6707
Danish Naseem <danishnaseem05@gmail.com>
2020-11-10 19:06:45 -0600

    added rspec tests

--- spec/controllers/piles_controller_spec.rb
+++ spec/controllers/piles_controller_spec.rb
@@ -10,12 +10,19 @@ describe PilesController do
       post :show, params
       expect(response).to redirect_to (:root)
     end
-    it 'should transfer card from source pile to destination pile' do
+    it 'should render the show page for the source pile' do
       params = {
           pile: {source_pile: "Deck"}
       }
       post :show, params
       expect(response).to render_template('show')
+    end
+    it 'should transfer selected card to the destination pile' do
+      params = {
+          pile: {source_pile: "Deck"}
+      }
+      post :show, params
+
     end
   end
 end
