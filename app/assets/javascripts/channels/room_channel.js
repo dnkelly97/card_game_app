ee0ad5767bfb1c3e192a4045eec71cc616fe4b08
Austin Strom <austin-strom@uiowa.edu>
2020-11-11 00:10:58 -0600

    Fixing js ES6 bug

--- app/assets/javascripts/channels/room_channel.js
+++ app/assets/javascripts/channels/room_channel.js
@@ -1,17 +1,17 @@
-import consumer from "./consumer"
+//import consumer from "./consumer"
 
-consumer.subscriptions.create(
-    {
-        channel: "RoomChannel",
-        id: window.location.href.split("/")[-1]
-    },
-    {
-        connected: function(data) {
-            console.log("Connected to socket. Reading " + data)
-        },
-        received: function(data) {
-            console.log("Received: " + data);
-            _onMessage ( data )
-        }
-    }
-)
+//consumer.subscriptions.create(
+//    {
+//        channel: "RoomChannel",
+//        id: window.location.href.split("/")[-1]
+//    },
+//    {
+//        connected: function(data) {
+//            console.log("Connected to socket. Reading " + data)
+//        },
+//        received: function(data) {
+//            console.log("Received: " + data);
+//            _onMessage ( data )
+//        }
+//    }
+//)
