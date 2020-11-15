8b73b9dce19119f37c8b6bdd3e3888b402cf35ff
Gabriel Douglass <gabrieljdouglass@gmail.com>
2020-11-15 15:32:20 -0600

    made it so Room has_many :users

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,5 @@
 class Room < ActiveRecord::Base
+  has_many :users
   #TODO: Add validations to handle invalid or empty inputs
   def self.create_room!(room_params)
     room_params[:room_code] = SecureRandom.random_number(9999999999)
