3da2bc8acb61310d3413950a2494c19ff374d857
Gabriel Douglass <gabrieljdouglass@gmail.com>
2020-11-15 17:47:26 -0600

    Made it so that the Room object has many Piles

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,6 @@
 class Room < ActiveRecord::Base
   has_many :users
+  has_many :piles
   #TODO: Add validations to handle invalid or empty inputs
   def self.create_room!(room_params)
     room_params[:room_code] = SecureRandom.random_number(9999999999)
