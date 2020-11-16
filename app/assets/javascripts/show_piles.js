dbc2a2bda781a3f955f75d348ec7d60f51dd52c2
Danish Naseem <danishnaseem05@gmail.com>
2020-11-16 15:12:05 -0600

    show_cards renamed to show_pile

--- app/assets/javascripts/show_piles.js
+++ app/assets/javascripts/show_piles.js
@@ -1,14 +1,8 @@
-function show_cards(id){
+function show_pile(id){
     let arr = document.querySelectorAll(".cardsTable tr")
     for(let i=0; i<arr.length; i++){
         arr.item(i).style.display = "none"
     }
     id = parseInt(id)
     document.getElementById(id).style.display = "block"
-}
-
-function display_pile(id){
-    var html_str = "<h6>Pile: " + id + "</h6>"
-    var panel = document.getElementById("show-pile-panel");
-    panel.innerHTML = html_str
 }
