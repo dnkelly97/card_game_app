3987c16b073e0653333dfa4971ddff961000fb44
dnkelly97 <dnkelly97@gmail.com>
2020-11-16 13:29:26 -0600

    created js function to display pile id in show pile panel

--- app/assets/javascripts/show_piles.js
+++ app/assets/javascripts/show_piles.js
@@ -6,3 +6,9 @@ function show_cards(id){
         document.getElementById(id).style.display = "block"
     }
 }
+
+function display_pile(id){
+    var html_str = "<h6>Pile: " + id + "</h6>"
+    var panel = document.getElementById("show-pile-panel");
+    panel.innerHTML = html_str
+}
