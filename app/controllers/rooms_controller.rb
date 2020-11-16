021268d918940579596b93d3d008968e7842e8f3
Gabriel Douglass <gabrieljdouglass@gmail.com>
2020-11-16 11:59:08 -0600

    commented stuff out hand made it so broken code redirects to homepage with flash indicating issue

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,5 @@
 class RoomsController < ApplicationController
+  #before_action :set_current_user
   def new
   end
 
@@ -7,20 +8,21 @@ class RoomsController < ApplicationController
                          :max_players => params[:room_max_players],
                          :game_type => params[:game_type],
                          :private => params[:private])
-    user = User.find_by_session_token(session[:session_token])
-    @room.users << user
+    #user = User.find_by_session_token(session[:session_token])
+    #@room.users << user
     @room.save!
     deck = Pile.find_by(:name => "Deck")
     @room.piles << deck
     @room.save!
     room = Room.find_by(:id => user.room_id)
-    flash[:notice] = "#{user.user_id}, welcome to #{room.name}"
+    flash[:notice] = "Welcome to your newly created room, #{room.name}"
     redirect_to room_path(@room)
   end
 
   def show
     id = params[:id]
     @room = Room.find(id)
+    @piles = Pile.all
   end
 
   def new_join
@@ -29,11 +31,11 @@ class RoomsController < ApplicationController
   def create_join
     begin
       @room = Room.find_by!(:room_code => params[:id])
-      user = User.find_by_session_token(session[:session_token])
-      @room.users << user
+      #@user = User.find_by_session_token(session[:session_token])
+      #@room.users << @user
       @room.save!
-      room = Room.find_by(:id => user.room_id)
-      flash[:notice] = "#{user.user_id}, welcome to #{room.name}"
+      #room = Room.find_by(:id => @user.room_id)
+      flash[:notice] = "Welcome to #{@room.name}"
       redirect_to room_path(@room)
     rescue ActiveRecord::RecordNotFound
       flash[:warning] = "A room with that code does not exist."
