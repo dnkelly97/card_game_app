b31180e62e34d893464fe7ce70310c02068f2498
Kevin Steele <kevsteele7@gmail.com>
2020-11-16 16:29:50 -0600

    Update content_security_policy.rb
    found it

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -16,7 +16,7 @@ Rails.application.config.content_security_policy do |policy|
 
 #   # Specify URI for violation reports
 #   # policy.report_uri "/csp-violation-report-endpoint"
-# end
+end
 
 # If you are using UJS then enable automatic nonce generation
 # Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
