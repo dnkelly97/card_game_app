4a542e5d80091f3a39a49277eb5eec51fdd51a77
Austin Strom <austin-strom@uiowa.edu>
2020-11-16 21:56:25 -0600

    Adding base action buttons to action panel

---
+++ app/assets/javascripts/components/ActionsPanel.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+
+export default class ActionsPanel extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Button className="action-button" variant="outline-dark">Draw</Button>
+                <Button className="action-button" variant="outline-dark">Transfer</Button>
+                <Button className="action-button" variant="outline-dark">Reset Deck</Button>
+                <Button className="action-button" variant="outline-dark">Show Hand</Button>
+                <Button className="action-button" variant="outline-danger">Rest Game</Button>
+                <Button className="action-button" variant="outline-danger">Quit Game</Button>
+
+            </React.Fragment>
+        );
+    }
+}
