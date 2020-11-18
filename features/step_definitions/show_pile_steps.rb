8233330d9934613e9d0d0df90be5466b956f3e72
dnkelly97 <dnkelly97@gmail.com>
2020-11-17 19:31:57 -0600

    first cucumber feature outlined and some steps written

---
+++ features/step_definitions/show_pile_steps.rb
@@ -0,0 +1,15 @@
+
+Given /^that a private pile contains cards$/ do
+  Pile.create!(name: "private_pile_1", private_pile: true, creator: nil)
+  Card.create([{name: "Ace of Spades", pile_id: 1, unicode_value: "1F0A1"},
+               {name: "Two of Spades", pile_id: 1, unicode_value: "1F0A2"}])
+end
+
+When /^the pile button is clicked$/ do
+  visit rooms_show_piles_path
+  click_button 'private_pile_1'
+end
+
+Then(/^I should a number of facedown cards equal to the number of cards in the pile$/) do
+  pending
+end
