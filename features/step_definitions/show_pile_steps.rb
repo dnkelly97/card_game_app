fd2ba0f774b7a244c72514dee8e2b87e97b023f5
dnkelly97 <dnkelly97@gmail.com>
2020-11-19 22:25:53 -0600

    fixed uncommented line in cucumber test

--- features/step_definitions/show_pile_steps.rb
+++ features/step_definitions/show_pile_steps.rb
@@ -3,7 +3,7 @@ Given /^that a private pile contains cards$/ do
   pending
   # Pile.create!(name: "private_pile_1", private_pile: true, creator: nil)
   # Card.create([{name: "Ace of Spades", pile_id: 1, unicode_value: "1F0A1"},
-               {name: "Two of Spades", pile_id: 1, unicode_value: "1F0A2"}])
+  #              {name: "Two of Spades", pile_id: 1, unicode_value: "1F0A2"}])
 end
 
 When /^the pile button is clicked$/ do
