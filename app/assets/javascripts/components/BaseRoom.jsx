080d07a7af6ebf9a7631b3fd4b4e8513b77e64e1
Austin Strom <austin-strom@uiowa.edu>
2020-11-19 22:10:21 -0600

    Adding multiplayer hand viewing demo so that the client has an idea of the direction that we are heading

--- app/assets/javascripts/components/BaseRoom.jsx
+++ app/assets/javascripts/components/BaseRoom.jsx
@@ -7,17 +7,25 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import '../../stylesheets/application.css';
 import CentralPiles from "./CentralPiles";
+import BlankCard from "./BlankCard";
+import {Button} from "react-bootstrap";
 
 export default class BaseRoom extends React.Component {
     render() {
         return (
             <div className="base-room">
                 <div className="left-panel">
-                    Other Players Cards
+                    <Button variant="outline-primary" className="blank-card">View Player 2's Hand</Button>
+                    <Button variant="outline-primary" className="blank-card">View Player 3's Hand</Button>
+                    <Button variant="outline-primary" className="blank-card">View Player 4's Hand</Button>
+                    <Button variant="outline-primary" className="blank-card">View Player 5's Hand</Button>
+                    <Button variant="outline-primary" className="blank-card">View Player 6's Hand</Button>
+                    <Button variant="outline-primary" className="blank-card">View Player 7's Hand</Button>
+                    <Button variant="outline-primary" className="blank-card">View Player 8's Hand</Button>
                 </div>
                 <div className="center-panel">
                     <div className="top-center-panel">
-                        Other Players Cards
+                        <h2>EXAMPLE: Interacting with players on the left column will show a more detailed view of their hands here</h2>
                     </div>
                     <div className="middle-center-panel">
                         <CentralPiles/>
