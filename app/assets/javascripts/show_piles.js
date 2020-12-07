32e593771fd988e7595564b4dbbed00a7b1934e5
Danish Naseem <danishnaseem05@users.noreply.github.com>
2020-12-07 17:07:03 -0600

    Moved the javascript code from the show.html.erb to app/assets/show_piles.js

--- app/assets/javascripts/show_piles.js
+++ app/assets/javascripts/show_piles.js
@@ -1,11 +1,8 @@
-// function show_pile(id){
-//     console.log("made it to show_pile")
-//     let arr = document.querySelectorAll(".cardsTable tr")
-//     for(let i=0; i<arr.length; i++){
-//         arr.item(i).style.display = "none"
-//     }
-//     id = parseInt(id)
-//     document.getElementById(id).style.display = "block"
-// }
-
-// Above code moved to show_piles.html.erb for now because importing wasn't working
+function show_pile(id){
+    let arr = document.querySelectorAll(".cardsTable tr")
+    for(let i=0; i<arr.length; i++){
+        arr.item(i).style.display = "none"
+    }
+    id = parseInt(id)
+    document.getElementById(id).style.display = "block"
+}
