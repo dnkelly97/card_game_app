6a7d7aaa9cfccb50ff6dcaf3186238d845c4a5a9
Danish Naseem <danishnaseem05@users.noreply.github.com>
2020-12-07 17:30:43 -0600

    added some code but yet to be completed. Calling the javascript function show_piles with some id is yet to be tested in jasmine

---
+++ spec/javascripts/rooms_spec.js
@@ -0,0 +1,6 @@
+describe('ShowPile', function(){
+    it('Should display the cards in that pile with the click of the button', function(){
+        //TODO: make a fixture with buttons and a panel
+        // Then call the js function show_piles with the pile id
+    })
+})
