20ba21fdde6a0eb3c6efc8c3f99c7e0a1aa04d19
dnkelly97 <dnkelly97@gmail.com>
2020-12-11 21:53:23 -0600

    fixed a testing issue and added an end statement in _show_game_room where I left one out in a merge.

--- spec/controllers/rooms_controller_spec.rb
+++ spec/controllers/rooms_controller_spec.rb
@@ -93,9 +93,11 @@ describe RoomsController do
     end
   end
   describe 'RoomsController#show' do
-    it 'should use the id parameter to assign the correct room to @rooms, making the room available to the view' do
+    before(:each) do
       allow(SecureRandom).to receive(:random_number).and_return(1111111111)
       post :create, params: {room_name: "Dan's Test Room", max_players: 1}, session: {session_token: @user.session_token}
+    end
+    it 'should use the id parameter to assign the correct room to @rooms, making the room available to the view' do
       get :show, params: {id: Room.all[0].id}
       expect(assigns(:room)).to eq(Room.find_by_id(Room.all[0].id))
     end
