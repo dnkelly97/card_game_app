8a532e4a88e7c48fa5087a9592f354c974f79bfa
KaiwenS7 <kevsteele7@gmail.com>
2020-12-12 22:41:07 -0600

    added cable URI

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.action_cable.disable_request_forgery_protection = true
+  config.web_socket_server_url = "wss://g003-cardgamesim-sprint3.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins =  ["https://g003-cardgamesim-sprint3.herokuapp.com",
+                                               %r{https://g003-cardgamesim-sprint3.herokuapp.com/*}]
 end
