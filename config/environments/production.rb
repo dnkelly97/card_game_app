134a5f57cc8e3b899e928b419cdcd2feafd221fa
KaiwenS7 <kevsteele7@gmail.com>
2020-12-12 22:26:42 -0600

    allow requests for all origins for testing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_cable.allowed_request_origins =  ["https://g003-cardgamesim-sprint3.herokuapp.com",
-                                               %r{https://g003-cardgamesim-sprint3.herokuapp.com/*}]
+  config.action_cable.disable_request_forgery_protection = true
 end
