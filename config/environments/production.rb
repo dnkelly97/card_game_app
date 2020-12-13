631661ccc4a98f03a2a6954935628a773d9abfc8
KaiwenS7 <kevsteele7@gmail.com>
2020-12-12 22:19:44 -0600

    added action cable accepted locations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_cable.allow_request_origins = ["https://g003-cardgamesim-sprint3.herokuapp.com",
+  config.action_cable.allowed_request_origins =  ["https://g003-cardgamesim-sprint3.herokuapp.com",
                                                %r{https://g003-cardgamesim-sprint3.herokuapp.com/*}]
 end
